c6da072663e1b8ffe4132923bf93d405481b230a
Job Okello <jobokello5@gmail.com>
2020-04-23 18:23:41 +0300

    Getting started with java full path

---
+++ 1. Getting Started/src/com/jobokello/Main.java
@@ -0,0 +1,9 @@
+package com.jobokello;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+     System.out.println("Hello Job Welcome to Java");
+    }
+}
